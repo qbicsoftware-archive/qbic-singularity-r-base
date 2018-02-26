66d5098e107dda236d75c9dd730ce6e3d1ad24d4
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-02-26 11:14:06 +0100

    Prepare template

--- build.sh
+++ build.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
-#This is required to build the stuff
+# Define the path to the rpackage file list
+# Just a line separated list of package names,
+# which can be resolved by Bioconductor
+PKGLIST=/tmp/rpackages.txt
 
-RMAJOR=3
-RVERSION=3.2.3-4
+# Updae and upgrade the OS
+apt update && apt upgrade -y 
+# Install additional debian-based backages
+apt install -y git ca-certificates
 
-#Add here what kind of dependencies you need in general
-#Check for packages here: https://pkgs.alpinelinux.org/packages
-apt update && apt upgrade -y && apt install -y bash wget curl gcc git ca-certificates r-base=$RVERSION libssl-dev libcurl4-openssl-dev libxml2-dev openjdk-8-jre
-
-# Download nlopt, as this will be needed by some r packages
+# Optional build directory, if you need to compile
+# something from source
 mkdir /build
 cd /build
 
 # Download and install biocLite core packages
-sh -c 'R --vanilla <<EOF
+cat $PKGLIST | xargs -i sh -c 'R --vanilla <<EOF
 source("https://bioconductor.org/biocLite.R")
-biocLite()
+biocLite("{}", ask=FALSE)
 q()
 EOF'
 
-#Create mountpoints that we need for CFC
+# Create internal mountpoints that we need for CFC until we
+# have a kernel with OverlayFS support
 mkdir -p /sfs/7
 
-#Clean up
-rm -rf /var/cache/apk/*
+# Clean up
+apt clean
 
-cd /
+# Delete build directory
 rm -rf /build
